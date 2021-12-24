49742cbd12d57b5874aa8741e472839a17e3e43e
mqifjjfwiot <85283021+mqifjjfwiot@users.noreply.github.com>
2021-12-24 09:43:33 +0900

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,7 +1,10 @@
 import Data_Processing
 
-#Data_Processing.subway_data_processing()
 #Data_Processing.total_data_processing()
+
+
 #Data_Processing.population_data()
 #Data_Processing.traffic_data()
-Data_Processing.cafe_data()
+#Data_Processing.cafe_data()
+#Data_Processing.culture_data()
+#Data_Processing.area_data()
